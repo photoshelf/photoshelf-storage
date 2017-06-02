36e43a44a5b6f338c5db40292f37a6f1e3311fee
maeda <duck8823@gmail.com>
2017-06-03 01:30:45 +0900

    Initial commit

---
+++ server.go
@@ -0,0 +1,12 @@
+package main
+
+import "net/http"
+import "github.com/labstack/echo"
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+}

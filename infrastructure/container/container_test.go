99ba3b79754f13b29132a35f00475c31d868f202
shunsuke maeda <duck8823@gmail.com>
2017-11-11 12:48:19 +0900

    Add test data

--- infrastructure/container/container_test.go
+++ infrastructure/container/container_test.go
@@ -18,33 +18,57 @@ type testdata struct {
 	val interface{}
 }
 
-var testdataSet = []testdata{
-	{new(string), "value"},
-	{new(int), 1234},
-	{new(float64), 12.34},
-	{new(testtype), testtype{"hoge", "fuga"}},
-	{new(testtype), &testtype{"hoge", "fuga"}},
+type testInterface interface {
+	Hoge()
 }
 
+type testInterfaceTwo interface {
+	Hoge()
+}
+
+type testImpl struct {
+	Name string
+}
+
+func (*testImpl) Hoge() {}
+
+var interfaceVar *testInterface = new(testInterface)
+
+var noneInterface *testInterfaceTwo = new(testInterfaceTwo)
+
+var testdataSet []testdata
+
 func TestMain(m *testing.M) {
+	testdataSet = []testdata{
+		{new(string), "value"},
+		{new(int), 1234},
+		{new(float64), 12.34},
+		{new(testtype), testtype{"hoge", "fuga"}},
+	}
+
 	for _, testdata := range testdataSet {
 		Set(testdata.val)
 	}
+	Set(&testImpl{"hoge"})
 	os.Exit(m.Run())
 }
 
 func TestContainer(t *testing.T) {
 	type hoge string
+
 	testdataSet = append(testdataSet, testdata{new(hoge), nil})
+	testdataSet = append(testdataSet, testdata{new(testImpl), testImpl{"hoge"}})
+	testdataSet = append(testdataSet, testdata{new(testInterface), &testImpl{"hoge"}})
+	testdataSet = append(testdataSet, testdata{new(testInterfaceTwo), nil})
 
 	for _, testdata := range testdataSet {
-		t.Run(fmt.Sprintf("type=%s", reflect.TypeOf(testdata.emp).Name()), func(t *testing.T) {
+		t.Run(fmt.Sprintf("type=%s", reflect.TypeOf(testdata.emp).String()), func(t *testing.T) {
 			a := testdata.emp
 			Get(a)
 
 			if testdata.val != nil {
 				actual := reflect.Indirect(reflect.ValueOf(a)).Interface()
-				expect := reflect.Indirect(reflect.ValueOf(testdata.val)).Interface()
+				expect := reflect.ValueOf(testdata.val).Interface()
 
 				assert.EqualValues(t, expect, actual)
 			} else {

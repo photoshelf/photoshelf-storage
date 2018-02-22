16955a82c78868b61f882487627d7dd3f84f777b
maeda <duck8823@gmail.com>
2018-02-23 00:01:03 +0900

    Add test case

--- presentation/router/router_test.go
+++ presentation/router/router_test.go
@@ -4,12 +4,14 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/photoshelf/photoshelf-storage/infrastructure/container"
 	"github.com/photoshelf/photoshelf-storage/presentation/mock_controller"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func TestLoad(t *testing.T) {
+func TestLoadEchoServer(t *testing.T) {
 
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
@@ -64,4 +66,10 @@ func TestLoad(t *testing.T) {
 			t.Fatal(err)
 		}
 	})
+}
+
+func TestLoadGrpcServer(t *testing.T) {
+	s := LoadGrpcServer()
+
+	assert.IsType(t, &grpc.Server{}, s)
 }
